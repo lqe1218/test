0c9dcaabe58ba1de2f420b6183f85e497df73bdf
Qiu E <qiue@simitgroup.com>
2024-03-31 13:44:29 +0800

    add countdown timer

--- project/components/page/header.vue
+++ project/components/page/header.vue
@@ -3,7 +3,7 @@
         <div class="header-left">
             <img class="logoimg" src="assets/img/logo-white.png" alt="logo"/>
             Promotion Ends In
-            <CountDownTime :data="data"/>
+            <CountDownTime v-if="data" :data="data"/>
         </div>
         <div class="header-right">
             <Button @click="register" label-name="Register"/> 
@@ -30,6 +30,11 @@ import CountDownTime from '../text/CountDownTime.vue';
 import Button from '../button/button.vue';
 import userStore from '../../store/userStore';
 import Dialog from 'primevue/dialog';
+import { onMounted } from 'vue';
+
+
+const targetDate = new Date('2024-02-29');
+// const currentDate = ref(new Date());
 
 
 const formStore = userStore();
@@ -38,12 +43,24 @@ const loginData = ref({
     password:''
 })
 
-let data={
-    day:"27",
-    hour:"23",
-    min:"45",
-    sec:"07"
+// let data={
+//     day:"27",
+//     hour:"23",
+//     min:"45",
+//     sec:"07"
+// }
+const data =ref();
+const updateDateTime = () => {
+    let currentDate=new Date();
+    const difference = targetDate.getTime() -currentDate.getTime();
+    data.value = {
+        day:Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hour: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        min: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+        sec: Math.floor((difference % (1000 * 60)) / 1000)
+    }
 }
+
 const visibleLogin=ref(false);
 
 const loginDialog = () => {
@@ -63,6 +80,9 @@ const register = () =>{
         behavior: 'smooth'
     });
 }
+onMounted(()=>{
+    setInterval(updateDateTime, 1000);
+})
 </script>
 
 <style>
